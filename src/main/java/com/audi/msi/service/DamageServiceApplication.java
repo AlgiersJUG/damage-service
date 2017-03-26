62bf27960c945f623195c0bb0ce3a6d9c54894ec
Jpuzzler Jpuzzler <Jpuzzler Jpuzzler>
2017-03-26 19:07:59 +0200

    Initial Import

---
+++ src/main/java/com/audi/msi/service/DamageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.audi.msi.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamageServiceApplication.class, args);
+	}
+}
